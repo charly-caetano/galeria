da584e43cf893a0c6c3e9f60ac6c37893fa01baa
charly-caetano <charly.caetano@gmail.com>
2026-04-16 21:45:32 -0300

    Delete galeria de imagem directory

--- galeria de imagem/script.js
+++
@@ -1,9 +0,0 @@
-// Função que troca a imagem principal da galeria
-function trocarImagem(elemento) {
-  // Busca no HTML a imagem principal da galeria
-  let imagemGrande = document.getElementById("imagemGrande");
-  if (!imagemGrande) return;
-
-  // A miniatura usa 150x100 e a imagem grande usa 600/400
-  imagemGrande.src = elemento.src.replace("150/100", "600/400");
-}
